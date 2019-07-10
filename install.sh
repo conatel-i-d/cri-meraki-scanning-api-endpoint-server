a122f702ed470583363d7026124f16b701812fa8
Guzman Monne <gmonne@conatel.com.uy>
2019-07-10 20:11:55 +0000

    fixed bugs with install

--- install.sh
+++ install.sh
@@ -5,14 +5,13 @@ MERAKI_ENDPOINT_VERSION=${MERAKI_ENDPOINT_VERSION:-"0.0.2"}
 echo "Creating user meraki_endpoint"
 sudo useradd meraki_endpoint -s /sbin/nologin -M
 echo "Moving service configuration to /lib/systemd/system/"
-sudo cp ./meraki_endpoint.service /lib/systemd/system/.
+sudo mv ./meraki_endpoint.service /lib/systemd/system/.
 sudo chmod 755 /lib/systemd/system/meraki_endpoint.service
 echo "Downloading meraki_endpoint binaries"
 wget https://github.com/guzmonne/meraki_endpoint/releases/download/$MERAKI_ENDPOINT_VERSION/meraki_endpoint
-sudo cp ./meraki_endpoint /usr/bin/meraki_endpoint
+sudo mv ./meraki_endpoint /usr/bin/meraki_endpoint
 echo "Creating application folders"
 sudo mkdir -p /srv/meraki_endpoint
 echo "Enable and start service"
 sudo systemctl enable meraki_endpoint.service 
 sudo systemctl start meraki_endpoint.service 
-sudo journalctl -f -u meraki_endpoint
